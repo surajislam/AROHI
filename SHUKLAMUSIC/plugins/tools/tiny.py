4d294da09e94741485edf86560218d18566d5e0f
⌯ ˹sʜɪᴠᴀɴsʜ ʜᴇʀᴇ˼ ™ ~ [ᴀғᴋ]↻🇮🇳 <149559899+SHIVANSH474@users.noreply.github.com>
2024-05-14 15:42:24 +0530

    Update tiny.py

--- SHUKLAMUSIC/plugins/tools/tiny.py
+++ SHUKLAMUSIC/plugins/tools/tiny.py
@@ -4,7 +4,7 @@ from pyrogram import Client, filters
 from SHUKLAMUSIC import app
 
 
-@app.on_message(filters.command("tiny"))
+@app.on_message(filters.command("shivtiny"))
 async def tiny_sticker(client, message):
     reply = message.reply_to_message
     if not (reply and reply.sticker):
