e49ea85f0e158abb1d3efb29b6eef636f782174a
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-21 15:06:31 +0530

    Update tringtring.py

--- SHUKLAMUSIC/plugins/Yumi/tringtring.py
+++ SHUKLAMUSIC/plugins/Yumi/tringtring.py
@@ -4,7 +4,7 @@ from SHUKLAMUSIC import app
 from pyrogram import filters
 from pyrogram.types import InputMediaPhoto
 
-@app.on_message(filters.command(["shivimage"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
+@app.on_message(filters.command(["image"], prefixes=["/", "!", "%", ",", "", ".", "@", "#"]))
 async def pinterest(_, message):
      chat_id = message.chat.id
 
