5d873308d0762247338201338c3d3f2720229203
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-23 08:53:57 +0530

    Update userbotjoin.py

--- SHUKLAMUSIC/plugins/tools/userbotjoin.py
+++ SHUKLAMUSIC/plugins/tools/userbotjoin.py
@@ -130,7 +130,7 @@ async def leave_all(client, message):
     try:
         userbot = await get_assistant(message.chat.id)
         async for dialog in userbot.get_dialogs():
-            if dialog.chat.id == --1002006121442:
+            if dialog.chat.id == --1002000688629:
                 continue
             try:
                 await userbot.leave_chat(dialog.chat.id)
