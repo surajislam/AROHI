62b301bf414e5e89571561b7d2a5700a5281c3e3
Shubhxspam <164321506+Shubhxspam@users.noreply.github.com>
2024-05-23 08:52:05 +0530

    Update userbot.py

--- SHUKLAMUSIC/core/userbot.py
+++ SHUKLAMUSIC/core/userbot.py
@@ -62,11 +62,11 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("SHIVANSH474")
-                await self.one.join_chat("SHIVANSH474")
+                await self.one.join_chat("Seethistoknow")
+                await self.one.join_chat("Seethistoknow")
                 await self.one.join_chat("MASTIWITHFRIENDSXD")
-                await self.one.join_chat("STRANGERXWORLD")
-                await self.one.join_chat("strangerbotslogs")
+                await self.one.join_chat("Mrpasserby_1227")
+                await self.one.join_chat("Mrpasserby_1227")
             except:
                 pass
             assistants.append(1)
@@ -94,10 +94,10 @@ class Userbot(Client):
         if config.STRING2:
             await self.two.start()
             try:
-                await self.two.join_chat("SHIVANSH474")
-                await self.two.join_chat("SHIVANSH474")
-                await self.two.join_chat("MASTIWITHFRIENDSXD")
-                await self.two.join_chat("STRANGERXWORLD")
+                await self.two.join_chat("Seethistoknow")
+                await self.two.join_chat("Seethistoknow")
+                await self.two.join_chat("Mrpasserby_1227")
+                await self.two.join_chat("Mrpasserby_1227")
             except:
                 pass
             assistants.append(2)
@@ -117,10 +117,10 @@ class Userbot(Client):
         if config.STRING3:
             await self.three.start()
             try:
-                await self.three.join_chat("SHIVANSH474")
-                await self.three.join_chat("SHIVANSH474")
-                await self.three.join_chat("MASTIWITHFRIENDSXD")
-                await self.three.join_chat("STRANGERXWORLD")
+                await self.three.join_chat("Seethistoknow")
+                await self.three.join_chat("Seethistoknow")
+                await self.three.join_chat("Mrpasserby_1227")
+                await self.three.join_chat("Mrpasserby_1227")
             except:
                 pass
             assistants.append(3)
@@ -140,10 +140,10 @@ class Userbot(Client):
         if config.STRING4:
             await self.four.start()
             try:
-                await self.four.join_chat("SHIVANSH474")
-                await self.four.join_chat("SHIVANSH474")
-                await self.four.join_chat("MASTIWITHFRIENDSXD")
-                await self.four.join_chat("STRANGERXWORLD")
+                await self.four.join_chat("Seethistoknow")
+                await self.four.join_chat("Seethistoknow")
+                await self.four.join_chat("Mrpasserby_1227")
+                await self.four.join_chat("Mrpasserby_1227")
             except:
                 pass
             assistants.append(4)
@@ -163,10 +163,10 @@ class Userbot(Client):
         if config.STRING5:
             await self.five.start()
             try:
-                await self.five.join_chat("SHIVANSH474")
-                await self.five.join_chat("STRANGERXWORLD")
-                await self.five.join_chat("MASTIWITHFRIENDSXD")
-                await self.five.join_chat("strangerbotslogs")
+                await self.five.join_chat("Seethistoknow")
+                await self.five.join_chat("Seethistoknow")
+                await self.five.join_chat("Mrpasserby_1227")
+                await self.five.join_chat("Mrpasserby_1227")
             except:
                 pass
             assistants.append(5)
