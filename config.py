01872ec85d691b18dc3b336c06cf5c14d784547e
surajislam <154444093+surajislam@users.noreply.github.com>
2024-05-30 19:36:06 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,10 +13,10 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "Navyaxprobot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "POLO_MUSIC_ROBOT")
 
 # Don't Add style font 
-BOT_NAME = getenv("BOT_NAME" , "❥⍣⃝𝐍ᴀᴠʏᴀⓍ≛⃝ 𝐃ᴇᴍᴏɴ🦋⃟❱‌ٖ")
+BOT_NAME = getenv("BOT_NAME" , "𓆩͟🌸𝆺꯭𝅥‌ٖٖٖٖٖٜ⃟ ͟🅝︎͕͜͝Ⓞ︎͕֟፝͜͝Ⓞ︎͕͜͜͝🅡︎͕͜͝ 𝆺꯭𝅥‌ٖٖٖٖٖٜ⃟🌸͟𓆪")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
@@ -36,19 +36,19 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
     # Don't edit variables below this line #
-SUDO_USERS = [2047725696]
+SUDO_USERS = [6727962511]
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/Shubhxspam/MUSIC",
+    "https://github.com/surajislam/AROHI",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/seethistoknow")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Mrpasserby_1227")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/anokhikeduniya")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/anokhikeduniya")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", True))
@@ -104,7 +104,7 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://telegra.ph/file/bfe04019445939fa049ac.png"
+    "START_IMG_URL", "https://graph.org/file/bd5a086516e474eb4757c.jpg"
 )
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/80e35d93784e239bdc86c.jpg"
