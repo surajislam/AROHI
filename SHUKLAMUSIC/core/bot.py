d0ce22da86d68c21c84c2ec630bbae23e77339c1
surajislam <154444093+surajislam@users.noreply.github.com>
2024-05-30 22:44:21 +0530

    Update bot.py

--- SHUKLAMUSIC/core/bot.py
+++ SHUKLAMUSIC/core/bot.py
@@ -6,11 +6,11 @@ import config
 from ..logging import LOGGER
 
 
-class SHUKLA(Client):
+class VIP(Client):
     def __init__(self):
         LOGGER(__name__).info(f"Starting Bot...")
         super().__init__(
-            name="SHUKLAMUSIC",
+            name="VIPMUSIC",
             api_id=config.API_ID,
             api_hash=config.API_HASH,
             bot_token=config.BOT_TOKEN,
@@ -34,22 +34,19 @@ class SHUKLA(Client):
             LOGGER(__name__).error(
                 "Bot has failed to access the log group/channel. Make sure that you have added your bot to your log group/channel."
             )
-            
+
         except Exception as ex:
             LOGGER(__name__).error(
                 f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
             )
-            
 
         a = await self.get_chat_member(config.LOGGER_ID, self.id)
         if a.status != ChatMemberStatus.ADMINISTRATOR:
             LOGGER(__name__).error(
                 "Please promote your bot as an admin in your log group/channel."
             )
-            
+
         LOGGER(__name__).info(f"Music Bot Started as {self.name}")
 
     async def stop(self):
         await super().stop()
-
-BOT_USERNAME = "@{self.username}"
